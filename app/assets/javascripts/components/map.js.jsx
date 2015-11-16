9a624c9e6cd52831e79fc2d56f6c3dab6007d5e6
Scott <dikowitz@gmail.com>
2015-11-16 10:43:49 -0500

    Add markers for each bench to map.

--- app/assets/javascripts/components/map.js.jsx
+++ app/assets/javascripts/components/map.js.jsx
@@ -1,11 +1,23 @@
 window.Map = React.createClass({
   componentDidMount: function () {
+    BenchStore.addChangeListener(this.createMarkers);
+
     var map = React.findDOMNode(this.refs.map);
     var mapOptions = {
       center: { lat: 40.750388, lng: -73.985870},
       zoom: 13
     };
     this.map = new google.maps.Map(map, mapOptions);
+  },
+
+  createMarkers: function () {
+    BenchStore.all().forEach(function (bench) {
+      new google.maps.Marker({
+        position: { lat: bench.lat, lng: bench.long },
+        map: this.map,
+        title: bench.description
+      });
+    }.bind(this));
   },
 
   render: function () {
